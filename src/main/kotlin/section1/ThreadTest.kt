a52b970d6ea4422a4832e608e12e7c7063a26ae0
Bullets-jyr <jyr@bullets.co.kr>
2023-03-06 13:12:06 +0900

    11-1 동시성 프로그래밍

---
+++ src/main/kotlin/section1/ThreadTest.kt
@@ -0,0 +1,39 @@
+package section1
+
+// 클래스
+class SimpleThread : Thread() {
+
+    override fun run() {
+        println("Class Thread ${Thread.currentThread()}")
+    }
+}
+
+// 인터페이스
+class SimpleRunnable : Runnable {
+
+    override fun run() {
+        println("Interface Thread ${Thread.currentThread()}")
+    }
+}
+
+fun main() {
+    val thread = SimpleThread()
+
+    thread.start()
+
+    val runnable = SimpleRunnable()
+    val thread2 = Thread(runnable)
+    thread2.start()
+
+    // 익명객체
+    object : Thread() {
+        override fun run() {
+            println("object Thread ${Thread.currentThread()}")
+        }
+    }.start()
+
+    // 람다식
+    Thread {
+        println("Lambda Thread ${Thread.currentThread()}")
+    }.start()
+}
